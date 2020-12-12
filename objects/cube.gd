e45f1301bb35b48bef5ce4f16a6a9931349f334c
Christian <christian.walter@outlook.at>
2020-12-12 03:29:12 +0100

    path following

--- objects/cube.gd
+++ objects/cube.gd
@@ -1,5 +1,23 @@
 extends Spatial
 
-
 func _on_Area_area_entered(area):
 	print("entered: ", area)
+	
+	
+export var path_id = 0
+export var speed = 10
+
+var actual_path
+var path_container
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	actual_path = get_node("cube_path")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	# update position on path
+	actual_path.get_child(0).set_offset(actual_path.get_child(0).get_offset() + speed * delta)
---
+++ objects/cube_v2.gd
@@ -0,0 +1,28 @@
+extends Spatial
+
+export var path_id = 0
+export var speed = 10
+
+var actual_path
+var path_container
+
+# ready
+func _ready():
+	
+	actual_path = get_node("actual_path")
+	path_container = get_node("path_container")
+
+	# get path to follow
+	actual_path.curve = path_container.get_child(path_id).curve
+	
+
+# update
+func _process(delta):
+
+	# update position on path
+	actual_path.get_child(0).set_offset(actual_path.get_child(0).get_offset() + speed * delta)
+
+	
+# area enter
+func _on_Area_area_entered(area):
+	print("body entered: ", area)
